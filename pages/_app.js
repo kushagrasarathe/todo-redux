f02a210be72dffd8b845b4fdce45baec76590747
kushagrasarathe <76868364+kushagrasarathe@users.noreply.github.com>
2023-04-10 22:12:33 +0530

    added protected routes

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,21 @@
-import '@/styles/globals.css'
+import ProtectedRoute from "@/src/config/ProtectedRoute";
+import { AuthContextProvider } from "@/src/context/AuthContext";
+import "@/styles/globals.css";
+import { useRouter } from "next/router";
+
+const noAuthReq = ["/", "/login", "/signup"];
 
 export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  const router = useRouter();
+  return (
+    <AuthContextProvider>
+      {noAuthReq.includes(router.pathname) ? (
+        <Component {...pageProps} />
+      ) : (
+        <ProtectedRoute>
+          <Component {...pageProps} />
+        </ProtectedRoute>
+      )}
+    </AuthContextProvider>
+  );
 }
--- src/context/AuthContext.js
+++ src/context/AuthContext.js
@@ -1,43 +1,46 @@
-import { createUserWithEmailAndPassword, onAuthStateChanged, signInWithEmailAndPassword } from "firebase/auth";
+import {
+  createUserWithEmailAndPassword,
+  onAuthStateChanged,
+  signInWithEmailAndPassword,
+} from "firebase/auth";
 import { auth } from "../config/firebaseConfig";
-
 const { createContext, useContext, useState, useEffect } = require("react");
 
-const AuthContext = createContext({})
+const AuthContext = createContext({});
 
-export const useAuth = () => useContext(AuthContext)
+export const useAuth = () => useContext(AuthContext);
 
-export function AuthContextProvider({ children }) {
-    const [loading, setLoading] = useState(false)
-    const [user, setUser] = useState(true)
+export const AuthContextProvider = ({ children }) => {
+  const [loading, setLoading] = useState(false);
+  const [user, setUser] = useState(true);
 
-    useEffect(() => {
-        const unsubscribe = onAuthStateChanged(auth, (user) => {
-            if (user) {
-                setUser({
-                    uid: user.uid,
-                    email: user.email,
-                })
-            } else {
-                setUser(null)
-            }
-            setLoading(false)
-        })
+  useEffect(() => {
+    const unsubscribe = onAuthStateChanged(auth, (user) => {
+      if (user) {
+        setUser({
+          uid: user.uid,
+          email: user.email,
+        });
+      } else {
+        setUser(null);
+      }
+      setLoading(false);
+    });
 
-        return () => unsubscribe();
-    }, [])
+    return () => unsubscribe();
+  }, []);
 
-    const signUp = (email, password) => {
-        return createUserWithEmailAndPassword(auth, email, password)
-    }
+  const signUp = (email, password) => {
+    return createUserWithEmailAndPassword(auth, email, password);
+  };
 
-    const login = (email, password) => {
-        return signInWithEmailAndPassword(auth, email, password)
-    }
+  const login = (email, password) => {
+    return signInWithEmailAndPassword(auth, email, password);
+  };
 
-    return (
-        <AuthContext.Provider value={{ login, signUp, user }}>
-            {loading ? null : children}
-        </AuthContext.Provider>
-    )
-}
+  return (
+    <AuthContext.Provider value={{ login, signUp, user }}>
+      {loading ? null : children}
+    </AuthContext.Provider>
+  );
+};
